d9ffd3acc17773322fd1bdb7a1a98e03adcb656d
ronanboot <158291137+ronanboot@users.noreply.github.com>
2024-11-13 16:09:24 +0100

    Update monannonce.php

--- logement/monannonce.php
+++ logement/monannonce.php
@@ -8,45 +8,48 @@
 <!-- <?php include("../navbar.html"); ?> -->
 <body>
 
-    <div class="row">
-        <!-- Colonne Choix -->
-        <div class="col">
-            <div>
-                <h1>Annonce personnel</h1>
-                <ul>
-                    <li onclick="showInfo('Mannonce')">Mon annonce</li>
-                </ul>
-                <h1>Annonces aperçus</h1>
-                <ul>
-                    <li onclick="showInfo('Tannonce')">Annonce trouvé</li>
-                </ul>
-            </div>
-
+    <div class="col2">
+        <!-- Colonne Barre -->
+        <div class="col-nav">
+            <ul class="ul-col-nav">
+                <li class="li-col-nav">
+                    <a href="monannonce.php">Mes annonces</a>
+                </li>
+                <li class="li-col-nav">
+                    <a href="recherche.php">Recherche d'annonces</a>
+                </li>
+            </ul>
         </div>
 
-        <div class="col2">
-            <!-- Colonne Barre -->
-            <div class="col-nav">
-                <ul class="ul-col-nav">
-                    <li class="li-col-nav">
-                        <a href="monannonce.php">Mes annonces</a>
-                    </li>
-                    <li class="li-col-nav">
-                        <a href="recherche.php">Recherche d'annonces</a>
-                    </li>
-                </ul>
-            </div>
+        <!-- Colonne Infos -->
+        <div class="col" id="info-display">
 
-            <!-- Colonne Infos -->
-            <div class="col" id="info-display">
-                <h2>Informations</h2>
-                <p>Cliquez sur un des éléments à gauche pour afficher les informations.</p>
-            </div>
+            <?php
+                require_once '../admin/database.php';
+                $db = Database::connect();
+
+                $statement = $db->query('SELECT * FROM m_annonce');
+                $annonces = $statement->fetchAll();
+
+                foreach($annonces as $annonce)
+                {
+                    echo    '<ul>
+                                <il>Nom: ' . $annonce['nom'] .'</li>
+                                <il>Type: ' . $annonce['type'] .'</li>
+                                <il>Prix: ' . $annonce['prix'] .'</li>
+                                <il>Surface: ' . $annonce['surface'] .' m²</li>
+                                <il>Colocation: ' . $annonce['colocation'] .'</li>
+                                <il>Proximité: ' . $annonce['proxi'] .'</li>
+                                <il>Propriètaire: ' . $annonce['proprietaire'] .'</li>';
+                }
+                
+                Database::disconnect();
+            ?>
         </div>
     </div>
-
     <script src="script.js"></script>
-
 </body>
 
 </html>
+
+
