ec79c5b0fd1d8302e5fb145882c04a585fbb3e0b
Hawkfram <52381824+Hawkfram@users.noreply.github.com>
2024-11-13 21:33:37 +0100

    Avancement

--- Ressources/JS/main.js
+++ Ressources/JS/main.js
@@ -3,18 +3,60 @@
  */
 
 const btnPlus = document.querySelectorAll(".plus-icon");
-const elementsTopic = document.querySelectorAll(".help-element");
+if (btnPlus != null) {
+  const elementsTopic = document.querySelectorAll(".help-element");
 
-btnPlus.forEach((element, index) => {
-  element.addEventListener("click", () => {
-    let detailSection = elementsTopic[index].querySelector(".detail-section");
+  btnPlus.forEach((element, index) => {
+    element.addEventListener("click", () => {
+      let detailSection = elementsTopic[index].querySelector(".detail-section");
 
-    detailSection.classList.toggle("show");
+      detailSection.classList.toggle("show");
 
-    if (detailSection.classList.contains("show")) {
-      element.src = "Ressources/Images/arrow_up.svg";
-    } else {
-      element.src = "Ressources/Images/arrow_down.svg";
-    }
+      if (detailSection.classList.contains("show")) {
+        element.src = "../Ressources/Images/arrow_up.svg";
+      } else {
+        element.src = "../Ressources/Images/arrow_down.svg";
+      }
+    });
   });
-});
+}
+
+/*
+ * AUTHENTIFICATION JS
+ */
+const btnChoix = document.querySelectorAll(".connexion-header p");
+
+if (btnChoix != null) {
+  let formConnexion = document.getElementById("connexion-form");
+  let formInscription = document.getElementById("inscription-form");
+
+  btnChoix.forEach((element) => {
+    element.addEventListener("click", function (event) {
+      if (btnChoix[0].classList.contains("active")) {
+        btnChoix[0].classList.remove("active");
+        btnChoix[1].classList.add("active");
+        formConnexion.style.display = "none";
+        formInscription.style.display = "flex";
+      } else {
+        btnChoix[0].classList.add("active");
+        btnChoix[1].classList.remove("active");
+        formConnexion.style.display = "flex";
+        formInscription.style.display = "none";
+      }
+    });
+  });
+}
+
+/*
+ * TROC JSa
+ */
+const btnSection = document.querySelectorAll(".troc-header p");
+
+if (btnSection != null) {
+  btnSection.forEach((element) => {
+    element.addEventListener("click", () => {
+      btnSection[0].classList.toggle("active");
+      btnSection[1].classList.toggle("active");
+    });
+  });
+}
